aa1e55014a183c3820469c14262b3c3a41639ef1
Zhen Du <jenkin.du@gmail.com>
2017-03-20 16:45:18 -0400

    fix error after remove

--- src/comp6421/semantic/perform/CreateFunctionAction.java
+++ src/comp6421/semantic/perform/CreateFunctionAction.java
@@ -2,7 +2,7 @@ package comp6421.semantic.perform;
 
 import comp6421.scanner.Token;
 import comp6421.semantic.CompilerError;
-import comp6421.semantic.code.SpecialValues;
+//import comp6421.semantic.code.SpecialValues;
 import comp6421.semantic.entry.PrimitiveType;
 import comp6421.semantic.entry.VariableEntry;
 //import comp6421.semantic.expression.ExpressionContext;
@@ -20,10 +20,10 @@ public class CreateFunctionAction extends SymbolAction {
 			// Adding these pseudo-parameters ensures that all stack-frame offsets will make sense.
 			//
 			// '@' sign guarantees no name collisions
-			VariableEntry returnPcAddr    = new VariableEntry(SpecialValues.RETURN_ADDRESS_PARAMETER_NAME, new PrimitiveType("int"));
+//			VariableEntry returnPcAddr    = new VariableEntry(SpecialValues.RETURN_ADDRESS_PARAMETER_NAME, new PrimitiveType("int"));
 
 			//table.add(returnValueAddr);
-			context.currentSymbolTable.add(returnPcAddr);
+//			context.currentSymbolTable.add(returnPcAddr);
 
 //			ExpressionContext.setCurrentFunction(context.storedFunction);
 		}
