de8fcf00e6b123c7021620cf526e1180de662638
Zhen Du <jenkin.du@gmail.com>
2017-04-11 05:44:41 -0400

    fix bug

--- src/comp6421/semantic/migration/strategy/EndMigrationStrategy.java
+++ src/comp6421/semantic/migration/strategy/EndMigrationStrategy.java
@@ -2,13 +2,24 @@ package comp6421.semantic.migration.strategy;
 
 import comp6421.scanner.Token;
 import comp6421.semantic.SemanticException;
+import comp6421.semantic.migration.AdditionExpressionFragment;
+import comp6421.semantic.migration.ExpressionElement;
+import comp6421.semantic.migration.FunctionCallExpressionFragment;
 import comp6421.semantic.migration.MigrationStrategy;
+import comp6421.semantic.migration.MultiplicationExpressionFragment;
+import comp6421.semantic.migration.RelationExpressionFragment;
+import comp6421.semantic.migration.StatementBlock;
 
 public class EndMigrationStrategy extends MigrationStrategy {
 
 	@Override
 	public void execute(Token precedingToken) throws SemanticException {
-		context.finishTopElement();
+		ExpressionElement top = context.getCurrent();
+		if (top instanceof AdditionExpressionFragment || top instanceof StatementBlock
+				|| top instanceof FunctionCallExpressionFragment || top instanceof MultiplicationExpressionFragment
+				|| top instanceof RelationExpressionFragment) {
+			context.finishTopElement();
+		}
 	}
 
 }
